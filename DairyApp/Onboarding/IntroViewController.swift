83ddc865ec690c3a6c9120f7bd1a01fbf3d4af2e
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-05-07 14:47:55 +0500

    Onboarding is ready

--- DairyApp/Onboarding/IntroViewController.swift
+++ DairyApp/Onboarding/IntroViewController.swift
@@ -16,10 +16,10 @@ final class IntroViewController: UIViewController {
     private var button: UIButton = {
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.backgroundColor = UIColor(named: "button1")
+        button.backgroundColor = UIColor(named: "onboardingButton")
         button.setTitleColor(.white, for: .normal)
         button.setTitle("Пропустить", for: .normal)
-        button.layer.cornerRadius = 18
+        button.layer.cornerRadius = 20
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
@@ -27,35 +27,18 @@ final class IntroViewController: UIViewController {
     let scrollView = UIScrollView()
     let pageControll = UIPageControl()
     
-    let content = ["First", "Second", "Third"]
+    let content = ["Привет! Рады увидеть тебя в приложении Dairy! 🥳",
+                   "Ты можешь записывать все, что происходило с тобой за день 📑",
+                   "Ставить теги, прикреплять фото 🎆",
+                   "И тогда дневник подготовит для тебя статистику твоего самочувтвия 🫶"]
 
     //MARK: - Life cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = UIColor(named: "smallBackground")
-        button.addTarget(self, action: #selector(presentationWasViewed), for: .touchUpInside)
-        
         configurationScrollView()
-        view.addSubview(pageControll)
-        view.addSubview(button)
-        pageControll.numberOfPages = content.count
-        pageControll.pageIndicatorTintColor = UIColor(named: "button2")
-        pageControll.currentPageIndicatorTintColor = UIColor(named: "button1")
-        
-        
-        pageControll.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            pageControll.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -190),
-            pageControll.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            
-            button.topAnchor.constraint(equalTo: pageControll.bottomAnchor, constant: 50),
-            button.heightAnchor.constraint(equalToConstant: 50),
-            button.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
-            button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30)
-        ])
+        configurationSubView()
     }
    
     
@@ -74,7 +57,7 @@ final class IntroViewController: UIViewController {
     private func configurationScrollView() {
         scrollView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(scrollView)
-        scrollView.backgroundColor = UIColor(named: "backdround")
+        scrollView.backgroundColor = UIColor(named: "ondoardBackground")
         
         NSLayoutConstraint.activate([
             scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
@@ -82,27 +65,52 @@ final class IntroViewController: UIViewController {
             scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
-        scrollView.contentSize = CGSize(width: view.bounds.width * 3, height: view.bounds.height)
+        scrollView.contentSize = CGSize(width: view.bounds.width * 4, height: view.bounds.height)
         scrollView.isPagingEnabled = true
         scrollView.delegate = self
         
         addLabel(title: content[0], position: 0)
         addLabel(title: content[1], position: 1)
         addLabel(title: content[2], position: 2)
+        addLabel(title: content[3], position: 3)
         
     }
     
     private func addLabel(title: String, position: CGFloat) {
         let label = UILabel()
         label.text = title
-        label.textColor = .label
+        label.textColor = UIColor(named: "textColor")
         label.textAlignment = .center
         label.numberOfLines = 0
-        label.font = UIFont.systemFont(ofSize: 18, weight: .heavy)
+        label.font = UIFont.systemFont(ofSize: 23, weight: .heavy)
         scrollView.addSubview(label)
         let width = view.bounds.width
         label.frame = CGRect(x: width * position, y: view.bounds.height / 3, width: width, height: 200)
         
+    }
+    
+    private func configurationSubView() {
+        
+        view.backgroundColor = UIColor(named: "ondoardBackground")
+        button.addTarget(self, action: #selector(presentationWasViewed), for: .touchUpInside)
+        
+        view.addSubview(pageControll)
+        view.addSubview(button)
+        
+        pageControll.numberOfPages = content.count
+        pageControll.pageIndicatorTintColor = .white
+        pageControll.currentPageIndicatorTintColor = UIColor(named: "onboardingButton")
+        pageControll.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            pageControll.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -190),
+            pageControll.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            button.topAnchor.constraint(equalTo: pageControll.bottomAnchor, constant: 50),
+            button.heightAnchor.constraint(equalToConstant: 50),
+            button.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30)
+        ])
     }
 }
 
@@ -112,7 +120,5 @@ extension IntroViewController: UIScrollViewDelegate {
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         pageControll.currentPage = Int(scrollView.contentOffset.x / view.bounds.width)
-        
-        
     }
 }
